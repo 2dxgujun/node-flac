34ed65a02e3edd64a64f8492cba3a351c2b2910e
Jun Gu <2dxgujun@gmail.com>
2018-04-25 14:40:25 +0800

    dev stash

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 var bindings = require('bindings')('flac-bindings')
+var fs = require('fs')
 
-console.log(typeof bindings.metadata.SimpleIteratorStatusString[0])
-
-for (let i in bindings.metadata.SimpleIteratorStatus) {
-  console.log(typeof bindings.metadata.SimpleIteratorStatus[i])
-  console.log(i)
+function take(num) {
+  if (num == 0) return
+  fs.readFileSync('/Users/2dxgujun/Desktop/320.mp3')
+  console.log(process.memoryUsage().rss / 1024 / 1024)
+  setTimeout(() => {
+    take(--num)
+  }, 1000)
 }
-//console.log(bindings.metadata.SimpleIteratorStatus)
+
+take(15)
+
+function dump() {
+  console.log(`dump: ${JSON.stringify(process.memoryUsage())}`)
+  setTimeout(dump, 1000)
+}
+
+dump()
+
+function _gc() {
+  gc()
+  setTimeout(_gc, 5000)
+}
+
+_gc()
+
+setTimeout(() => {}, 100000)
--- src/binding.cc
+++ src/binding.cc
@@ -6,10 +6,12 @@ using namespace node;
 
 NAN_MODULE_INIT(init_format);
 NAN_MODULE_INIT(init_metadata);
+NAN_MODULE_INIT(init_metadata_object);
 
 NAN_MODULE_INIT(init) {
   init_format(target);
   init_metadata(target);
+  init_metadata_object(target);
 }
 
 NODE_MODULE(flac_bindings, init);
